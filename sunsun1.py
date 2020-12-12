f9d61ad06b02a15dfe70d08795d83743e50b8bbf
saj <helga112@yandex.ru>
2020-12-12 17:27:47 +0300

    вторая  версия рисования

--- sunsun1.py
+++ sunsun1.py
@@ -31,10 +31,7 @@ class Example(QWidget):
     def draw_flag(self, qp):
         qp.setBrush(QColor(255, 0, 0))
         qp.drawRect(30, 30, 120, 30)
-        qp.setBrush(QColor(0, 255, 0))
-        qp.drawRect(30, 60, 120, 30)
-        qp.setBrush(QColor(0, 0, 255))
-        qp.drawRect(30, 90, 120, 30)
+        
 
 
 if __name__ == '__main__':
